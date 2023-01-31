7d5af2cb1d3c952bdefe357d74f4547c6008e6b3
mauricioMatuz <killashe96@gmail.com>
2023-01-31 06:10:42 -0600

    modificado a mysql

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize("centro_autobuses", "postgres", "1234", {
-  host: "localhost",
-  dialect: "postgres",
+export const sequelize = new Sequelize("centro_autobuses", "mau", "123", {
+  host: "54.160.108.127",
+  dialect: "mysql",
 });
