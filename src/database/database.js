407a7c30d1bec49c67b4b8d51567b14061e3c1ae
mauricioMatuz <killashe96@gmail.com>
2023-01-31 06:13:21 -0600

    ip elastica

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
 export const sequelize = new Sequelize("centro_autobuses", "mau", "123", {
-  host: "54.160.108.127",
+  host: "52.73.146.94",
   dialect: "mysql",
 });
